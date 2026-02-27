7617ba2962dbecd9666b697463e4d528c99b75ff
pankajakk <pankajakshi.k@gmail.com>
2026-02-27 17:18:17 +0000

    Initial Terraform project without state files

---
+++ createinstance.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "MyFirstInstnace" {
+count = 3  
+ami           = "ami-0b6c6ebed2801a5cb"
+  instance_type = "t2.micro"
+
+tags = {
+Name = "demoinstance-${count.index}"
+}
+}
